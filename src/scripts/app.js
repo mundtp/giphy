fdee09ac09e80461e3b4a68f7d99ec26cd501114
Philip Mundt <philipamundt@Philip-Mundts-MacBook-Pro-2.local>
2016-07-06 10:13:50 -0500

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,52 @@
-const React = require('react'),
-	ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
+import AppView from './AppView'
+
 
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
-		}
+	var GifCollection = Backbone.Collection.extend({
+	 url: 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC',
+	 _key: 'dc6zaTOxFJmzC',
+	 parse: function(rawJSON){
+	 return rawJSON.data
+	}
+
 	})
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+	var gifColl = new GifCollection
+
+
+	const GifRouter = Backbone.Router.extend({
+		routes: {
+			'search/:query': '_handleSearch',
+			'*anything': '_handleSearch'
+		},
+
+		initialize: function(){
+			Backbone.history.start()
+		},
+
+		_handleSearch: function (query){
+			gifColl.fetch({
+		data: {
+			api_key: gifColl._key,
+			q: query
+		}
+	})
+			ReactDOM.render(<AppView collection={gifColl}/>, document.querySelector('.container'))
+		}
+
+	})
+
+	new GifRouter()
 }
 
+
 app()
+
+
+
+
+
